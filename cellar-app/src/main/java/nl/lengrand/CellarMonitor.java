ea6cd84d6c11cb007336f73923acb54b88649920
Julien Lengrand-Lambert <julien@lengrand.fr>
2020-07-22 14:50:18 +0200

    Removing tests (for now) so app can be compiled on Mac

--- cellar-app/src/main/java/nl/lengrand/CellarMonitor.java
+++ cellar-app/src/main/java/nl/lengrand/CellarMonitor.java
@@ -9,26 +9,16 @@ public class CellarMonitor {
     private static final int THREADS = 1;
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(THREADS);
 
-    // From config
     private static final long START = 0;
     private static final long SPAN = 30;
     private static final TimeUnit UNIT = TimeUnit.MINUTES;
 
     private ScheduledFuture monitorHandle;
 
-    // How to use that exactly?
     private CellarProvider provider = new CellarProvider();
     private SensorApi faunaApi = new SensorApi();
 
-    final Runnable monitoring = () -> {
-        try {
-            faunaApi.add(provider.getSensorValues());
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-            System.out.println("Error add fauna data");
-            System.exit(0);
-        }
-    };
+    final Runnable monitoring = () -> { faunaApi.add(provider.getSensorValues()); };
 
     public void startMonitoring(){
         monitorHandle = scheduler.scheduleAtFixedRate(monitoring, START, SPAN, UNIT);
