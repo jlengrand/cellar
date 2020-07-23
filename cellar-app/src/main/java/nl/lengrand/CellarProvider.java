ce38717b5e4c23e32b5ce4ff094793148c1579eb
Julien Lengrand-Lambert <julien@lengrand.fr>
2020-07-23 14:39:20 +0200

    feat : generating valid FAT jars?

--- cellar-app/src/main/java/nl/lengrand/CellarProvider.java
+++ cellar-app/src/main/java/nl/lengrand/CellarProvider.java
@@ -1,15 +1,28 @@
 package nl.lengrand;
 
 import nl.lengrand.cellar.Dht11Driver;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class CellarProvider {
 
+    @ConfigProperty(name = "monitoring.enabled", defaultValue = "true")
+    private static boolean monitoringEnabled;
+
     private static final int N_TRIES = 5;
 
     private Dht11Driver driver = new Dht11Driver();
+
+    public CellarProvider(){
+        if(monitoringEnabled) {
+            System.out.println("Monitoring enabled by config. Starting up");
+            startMonitoring();
+        }
+        else System.out.println("Monitoring disabled by config");
+    }
+
 
     public SensorValue getSensorValues(){
 
@@ -26,5 +39,10 @@ public class CellarProvider {
 
     private boolean noSensorData(float[] values) {
         return values[0] == 0 && values[1] == 0;
+    }
+
+    static void startMonitoring(){
+        CellarMonitor monitor = new CellarMonitor();
+        monitor.startMonitoring();
     }
 }
