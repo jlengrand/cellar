7fe92c5d4ee48f2ef99e2a59accea4758d5c5b1a
Julien Lengrand-Lambert <julien@lengrand.fr>
2020-11-15 12:22:21 +0100

    Adds local config file

--- cellar-app/src/main/java/nl/lengrand/cellar/Main.java
+++ cellar-app/src/main/java/nl/lengrand/cellar/Main.java
@@ -1,15 +1,27 @@
 package nl.lengrand.cellar;
 
+import io.helidon.config.Config;
 import io.helidon.microprofile.server.Server;
 
-import java.io.IOException;
+import static io.helidon.config.ConfigSources.classpath;
+import static io.helidon.config.ConfigSources.file;
 
 public class Main {
 
-    private Main() {
+    private Main() {}
+
+    public static void main(final String[] args){
+        Server.builder()
+                .config(buildConfig())
+                .build().start();
     }
 
-    public static void main(final String[] args) throws IOException {
-        Server.create().start();
+    private static Config buildConfig() {
+        return Config.builder()
+                .disableEnvironmentVariablesSource()
+                .sources(
+                        file("cellar-config.properties").optional(),
+                        classpath("META-INF/microprofile-config.properties"))
+                .build();
     }
 }
