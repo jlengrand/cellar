2dd6eae075f41ab19b78ca47370c13e03ee7501c
Julien Lengrand-Lambert <julien@lengrand.fr>
2020-11-18 15:31:26 +0100

    Attempt to use Azure n1

--- cellar-influxdb/backup.sh
+++ cellar-influxdb/backup.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
+
 influx backup $BACKUP_NAME --token $CELLAR_ADMIN_TOKEN --host $CELLAR_HOST
+zip -r $BACKUP_NAME.zip $BACKUP_NAME
+az storage blob upload \
+    --account-name "cellarbackupstorage" \
+    --container-name "cellar-backups" \
+    --name $BACKUP_NAME.zip \
+    --file $BACKUP_NAME.zip \
+    --auth-mode login
